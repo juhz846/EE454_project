0724f60d3fbf1608d7d9122db345f2e5d0f4f8f3
Jalan Zhu <jjzhu@usc.edu>
2024-12-09 18:49:24 -0800

    Add files via upload

---
+++ CNN.v
@@ -0,0 +1,36 @@
+module Conv2d #(
+    parameter window_size,
+    parameter channels,
+    parameter neurons
+) (
+    
+    
+);
+    
+endmodule
+
+
+module MaxPool #(
+    parameters
+) (
+    ports
+);
+    
+endmodule
+
+module FC #(
+    parameters
+) (
+    ports
+);
+    
+endmodule
+
+
+module CNN #(
+    parameters
+) (
+    ports
+);
+    
+endmodule
